1ce483f8070b8a3eb85767c22e32a0ef3adc529e
Ridwan Lawal <80310167+Reazy-ai@users.noreply.github.com>
2023-10-11 17:34:13 +0100

    Add start_screen and gameover_screen

---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key, required this.hasGameStarted, required this.startGame});
+  final bool hasGameStarted;
+  final Function() startGame;
+
+  @override
+  Widget build(BuildContext context) {
+    return hasGameStarted? Container():Container(
+      alignment: Alignment(0,-0.2),
+      child: GestureDetector(
+          onTap: startGame,
+          child: Text("Tap to play")),
+    );
+  }
+}
