90af18a568a3fb8296aade404a485b17c8629ef8
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2023-10-11 17:41:53 +0100

    Increased text size

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key, required this.hasGameStarted, required this.startGame});
+  const StartScreen(
+      {super.key, required this.hasGameStarted, required this.startGame});
   final bool hasGameStarted;
   final Function() startGame;
 
   @override
   Widget build(BuildContext context) {
-    return hasGameStarted? Container():Container(
-      alignment: Alignment(0,-0.2),
-      child: GestureDetector(
-          onTap: startGame,
-          child: Text("Tap to play")),
-    );
+    return hasGameStarted
+        ? Container()
+        : Container(
+            alignment: Alignment(0, -0.2),
+            child: GestureDetector(
+                onTap: startGame,
+                child: Text(
+                  "Tap to play",
+                  style: TextStyle(fontSize: 24),
+                )),
+          );
   }
 }
