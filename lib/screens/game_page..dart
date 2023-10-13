45df6164bd99edb72501c45126c855546875aa66
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2023-10-13 20:44:04 +0100

    Tap to start and scores

--- lib/screens/game_page..dart
+++ lib/screens/game_page..dart
@@ -23,9 +23,14 @@ enum direction {
   RIGHT,
 }
 
+int score = 0;
+int brickNum = 0;
+int stage = 0;
+// Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){return newStage(stage: stage);}
+
 class _GamePageState extends State<GamePage> {
   //game started
-  bool hasGameStarted = false;
+  bool hasGameStarted = true;
   bool isGameOver = false;
 
   //ball position
@@ -80,7 +85,7 @@ class _GamePageState extends State<GamePage> {
 
   //start game function
   void startGame() {
-    hasGameStarted = true;
+    hasGameStarted = false;
     Timer.periodic(const Duration(milliseconds: 10), (timer) {
       moveBall();
       updateBallDirection();
@@ -89,6 +94,7 @@ class _GamePageState extends State<GamePage> {
       if (isPlayerDead()) {
         timer.cancel();
         isGameOver = true;
+        score;
       }
 
       //check if brick is broken
@@ -105,6 +111,12 @@ class _GamePageState extends State<GamePage> {
         setState(() {
           GameBricks[brick][2] = true;
           ballYDirection = direction.DOWN;
+          brickNum += 1;
+          if (brickNum == 4){
+            score += 1;
+            brickNum = 0;
+            stage += 1;
+          }
         });
       }
     }
@@ -167,7 +179,17 @@ class _GamePageState extends State<GamePage> {
           moveRight();
         }
       },
-      child: Scaffold(
+      child: hasGameStarted ? Scaffold(
+        backgroundColor: Colors.deepOrangeAccent,
+        body: Center(
+          child: StartScreen(
+                hasGameStarted: hasGameStarted,
+                startGame: () {
+                  startGame();
+                },
+              ),
+        )
+      ) : Scaffold(
         backgroundColor: Colors.deepOrangeAccent,
         body: Center(
           child: Stack(
@@ -194,14 +216,15 @@ class _GamePageState extends State<GamePage> {
                   iconSize: 30,
                 ),
               ),
-              StartScreen(
-                hasGameStarted: hasGameStarted,
-                startGame: () {
-                  startGame();
-                },
-              ),
+              // StartScreen(
+              //   hasGameStarted: hasGameStarted,
+              //   startGame: () {
+              //     startGame();
+              //   },
+              // ),
               GameOver(
                 isGameOver: isGameOver,
+                score: score,
               ),
 
               //bricks
