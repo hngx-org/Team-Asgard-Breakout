92555de3f6e28e189945aa6cc10095196edce059
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2023-10-20 13:08:54 +0100

    Breakout game

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
+import 'package:game_breakout_task/breakout_game.dart';
 import 'package:flutter/material.dart';
-import 'package:game_breakout_task/splash_screen.dart';
+import 'package:flame/game.dart';
 
 void main() {
-  runApp(const MyApp());
+  final game = BreakoutGame();
+  runApp(GameWidget(game: game));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SplashScreen(),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+
+// import 'ui/main_game_page.dart';
+
+// void main() {
+//   runApp(const BreakoutApp());
+// }
+
+// class BreakoutApp extends StatelessWidget {
+//   const BreakoutApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Breakout',
+//       home: MainGamePage(),
+//     );
+//   }
+// }
